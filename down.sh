6418e22817c5d87465b7066579d7b1d6bee78923
Logan Kimmel <logan@stackrox.com>
2021-03-17 22:51:17 -0700

    lotta changes

---
+++ down.sh
@@ -0,0 +1,10 @@
+#!/bin/bash +x
+
+export KOPS_FEATURE_FLAGS=AlphaAllowGCE
+export KOPS_STATE_STORE=gs://lkimmel-clusters
+kops delete cluster lk-test.k8s.local --yes
+
+argocd app delete prom-stack --cascade false
+argocd app delete elasticsearch --cascade false
+argocd app delete filebeat --cascade false
+argocd app delete kibana --cascade false
--- script.sh
+++ script.sh
@@ -11,17 +11,14 @@ kops create cluster lk-test.k8s.local \
 --vpc test-net
 kops validate cluster --wait 10m
 
-argocd app create prometheus --repo https://prometheus-community.github.io \
-  --helm-chart prometheus-community/prometheus --revision 13.6.0 --dest-namespace prometheus \
-  --dest-server https://34.70.219.120 --dest-namespace prometheus
+argocd cluster add lk-test.k8s.local
 
+kubectl apply -f prom-stack-manifest.yaml
+argocd app sync prom-stack
 
-#helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-#helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
-#helm install prometheus prometheus-community/prometheus -n prometheus --create-namespace
-#
-#helm repo add grafana https://grafana.github.io/helm-charts
-#helm repo update
-#
-#helm install my-release grafana/grafana
-#G_ADMIN=$(kubectl get secret --namespace default my-release-grafana -o jsonpath="{.data.admin-password}" | base64 -d)
+kubectl apply -f elasticsearch-manifest.yaml
+argocd app sync elasticsearch
+
+kubectl apply -f filebeat-manifest.yaml
+argocd app sync filebeat
+
