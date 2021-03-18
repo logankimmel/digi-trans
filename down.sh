ad5064d2ba2bda47bc10dd8d36a109b0d12e9302
Logan Kimmel <logan@stackrox.com>
2021-03-18 15:20:40 -0700

    hello-go

--- down.sh
+++ down.sh
@@ -1,8 +1,5 @@
-#!/bin/bash +x
-
-export KOPS_FEATURE_FLAGS=AlphaAllowGCE
-export KOPS_STATE_STORE=gs://lkimmel-clusters
-kops delete cluster lk-test.k8s.local --yes
+#!/bin/bash
+set -ex
 
 argocd app delete prom-stack --cascade=false
 argocd app delete elasticsearch --cascade=false
@@ -10,3 +7,15 @@ argocd app delete filebeat --cascade=false
 argocd app delete kibana --cascade=false
 argocd app delete traefik --cascade=false
 argocd app delete ingress --cascade=false
+
+server=$(argocd cluster list -o json | jq -r '.[] | select(.name=="lk-test.k8s.local") | .server')
+argocd cluster rm $server
+
+external_ip=$(kubectl --context lk-test.k8s.local get svc traefik -n traefik --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+gcloud dns record-sets transaction start --zone binbytes
+gcloud dns record-sets transaction remove "${external_ip}" --name '*.test.binbytes.io' --ttl 300 --type=A --zone=binbytes
+gcloud dns record-sets transaction execute --zone binbytes
+
+export KOPS_FEATURE_FLAGS=AlphaAllowGCE
+export KOPS_STATE_STORE=gs://lkimmel-clusters
+kops delete cluster lk-test.k8s.local --yes
--- up.sh
+++ up.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-set -e
-#
-#export KOPS_FEATURE_FLAGS=AlphaAllowGCE
-#export KOPS_STATE_STORE=gs://lkimmel-clusters
-#kops create cluster lk-test.k8s.local \
-#--zones us-central1-a \
-#--master-zones us-central1-a \
-#--node-count 2 \
-#--project lkimmel-1069 \
-#--yes \
-#--vpc test-net
-#kops validate cluster --wait 10m
-#
-#argocd cluster add lk-test.k8s.local
+set -ex
 
-# kubectl --context gke_customers-2_us-central1_logan-support-1  apply -f prom-stack-manifest.yaml
-# argocd app sync prom-stack
+export KOPS_FEATURE_FLAGS=AlphaAllowGCE
+export KOPS_STATE_STORE=gs://lkimmel-clusters
+kops create cluster lk-test.k8s.local \
+--zones us-central1-a \
+--master-zones us-central1-a \
+--node-count 2 \
+--project lkimmel-1069 \
+--yes \
+--vpc test-net
+kops validate cluster --wait 10m
+
+argocd cluster add lk-test.k8s.local
+
+gcloud config configurations activate stackrox
+gcloud container clusters get-credentials logan-support-1 --zone us-central1 --project customers-2
+
+kubectl --context gke_customers-2_us-central1_logan-support-1  apply -f prom-stack-manifest.yaml
+argocd app sync prom-stack
 
 kubectl --context gke_customers-2_us-central1_logan-support-1  apply -f elasticsearch-manifest.yaml
 argocd app sync elasticsearch
@@ -34,13 +37,15 @@ argocd app sync traefik
 external_ip=""
 while [ -z $external_ip ]; do                                                                                                                                                                
   echo "Waiting for end point..."
-  external_ip=$(kubectl get svc traefik -n traefik --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+  external_ip=$(kubectl --context lk-test.k8s.local get svc traefik -n traefik --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
   [ -z "$external_ip" ] && sleep 10
 done
 
+gcloud config configurations activate personal
 gcloud dns record-sets transaction start --zone binbytes
-gcloud dns record-sets transaction add 34.123.6.234 --name='*.test.binbytes.io' --ttl=300 --type=A --zone=binbyte
+gcloud dns record-sets transaction add $external_ip --name='*.test.binbytes.io' --ttl=300 --type=A --zone=binbytes
 gcloud dns record-sets transaction execute --zone binbytes
 
 kubectl  --context gke_customers-2_us-central1_logan-support-1  apply -f ingress-manifest.yaml
 argocd app sync ingress
+
