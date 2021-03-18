eab6e0bc89367f743541ac6bff56dab41ea81591
Logan Kimmel <logan@stackrox.com>
2021-03-18 01:55:23 -0700

    moar fixes

--- down.sh
+++ down.sh
@@ -4,7 +4,9 @@ export KOPS_FEATURE_FLAGS=AlphaAllowGCE
 export KOPS_STATE_STORE=gs://lkimmel-clusters
 kops delete cluster lk-test.k8s.local --yes
 
-argocd app delete prom-stack --cascade false
-argocd app delete elasticsearch --cascade false
-argocd app delete filebeat --cascade false
-argocd app delete kibana --cascade false
+argocd app delete prom-stack --cascade=false
+argocd app delete elasticsearch --cascade=false
+argocd app delete filebeat --cascade=false
+argocd app delete kibana --cascade=false
+argocd app delete traefik --cascade=false
+argocd app delete ingress --cascade=false
--- up.sh
+++ up.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
+set -e
+#
+#export KOPS_FEATURE_FLAGS=AlphaAllowGCE
+#export KOPS_STATE_STORE=gs://lkimmel-clusters
+#kops create cluster lk-test.k8s.local \
+#--zones us-central1-a \
+#--master-zones us-central1-a \
+#--node-count 2 \
+#--project lkimmel-1069 \
+#--yes \
+#--vpc test-net
+#kops validate cluster --wait 10m
+#
+#argocd cluster add lk-test.k8s.local
 
-export KOPS_FEATURE_FLAGS=AlphaAllowGCE
-export KOPS_STATE_STORE=gs://lkimmel-clusters
-kops create cluster lk-test.k8s.local \
---zones us-central1-a \
---master-zones us-central1-a \
---node-count 2 \
---project lkimmel-1069 \
---yes \
---vpc test-net
-kops validate cluster --wait 10m
-
-argocd cluster add lk-test.k8s.local
-
-kubectl --context gke_customers-2_us-central1_logan-support-1  apply -f prom-stack-manifest.yaml
-argocd app sync prom-stack
+# kubectl --context gke_customers-2_us-central1_logan-support-1  apply -f prom-stack-manifest.yaml
+# argocd app sync prom-stack
 
 kubectl --context gke_customers-2_us-central1_logan-support-1  apply -f elasticsearch-manifest.yaml
 argocd app sync elasticsearch
@@ -27,9 +28,11 @@ argocd app sync kibana
 
 kubectl --context lk-test.k8s.local create ns traefik
 kubectl --context gke_customers-2_us-central1_logan-support-1 get secret binbytes-dns -n traefik -o yaml  | kubectl --context lk-test.k8s.local apply -n traefik -f -
+kubectl --context gke_customers-2_us-central1_logan-support-1 apply -f traefik-manifest.yaml
+argocd app sync traefik
 
 external_ip=""
-while [ -z $external_ip ]; do                                                                                                                                                                [±main ●●]
+while [ -z $external_ip ]; do                                                                                                                                                                
   echo "Waiting for end point..."
   external_ip=$(kubectl get svc traefik -n traefik --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
   [ -z "$external_ip" ] && sleep 10
